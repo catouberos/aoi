6f694caf33244c794341bf2bbf69252825dcf2c5
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-29 23:29:23 +0700

    feat: implements caching

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use aoi::playing::now_playing;
 use axum::{
     extract::{Path, State},
     http::header,
@@ -8,9 +9,8 @@ use axum::{
     Router,
 };
 use base64::{engine::general_purpose, Engine};
-use listenbrainz::raw::{response::UserPlayingNowListen, Client};
+use listenbrainz::raw::Client;
 use moka::future::Cache;
-use musicbrainz_rs::{entity::release_group::ReleaseGroup, Browse};
 use reqwest::StatusCode;
 use resvg::{render, tiny_skia::Pixmap};
 use tera::{Context, Tera};
@@ -92,8 +92,11 @@ async fn get_playing_now(
     let image_data = reqwest::get(&image).await.unwrap().bytes().await.unwrap();
     let image_encoded = general_purpose::STANDARD.encode(&image_data);
 
-    context.insert("title", &track.track_metadata.track_name);
-    context.insert("artist", &track.track_metadata.artist_name);
+    let track_name = track.track_metadata.track_name;
+    let artist_name = track.track_metadata.artist_name;
+
+    context.insert("title", &track_name);
+    context.insert("artist", &artist_name);
     context.insert(
         "image",
         &format!("data:image/jpeg;base64,{}", &image_encoded),
@@ -132,50 +135,4 @@ async fn get_playing_now(
         ],
         result,
     ))
-}
-
-async fn now_playing(
-    client: Client,
-    user: String,
-) -> Result<(UserPlayingNowListen, ReleaseGroup), String> {
-    let now_playing = match client.user_playing_now(&user) {
-        Ok(val) => val,
-        Err(err) => return Err(format!("Error while getting user now playing: {}", err)),
-    };
-
-    println!("{:#?}", now_playing);
-
-    let listen = match now_playing.payload.listens.first() {
-        Some(val) => val,
-        None => return Err(format!("User [{}] does not have any listen", user)),
-    };
-
-    let release_id = match listen.track_metadata.additional_info.get("release_mbid") {
-        Some(val) => match val.as_str() {
-            Some(val) => val,
-            None => return Err(format!("Cannot parse track release ID")),
-        },
-        None => {
-            return Err(format!(
-                "Track [{}] does not have a release ID",
-                listen.track_metadata.track_name
-            ))
-        }
-    };
-
-    let results = match ReleaseGroup::browse()
-        .by_release(release_id)
-        .execute()
-        .await
-    {
-        Ok(val) => val,
-        Err(err) => return Err(format!("Error while browsing release group: {}", err)),
-    };
-
-    let release_group = match results.entities.first() {
-        Some(val) => val,
-        None => return Err(format!("Error while getting release group")),
-    };
-
-    Ok((listen.clone(), release_group.clone()))
 }

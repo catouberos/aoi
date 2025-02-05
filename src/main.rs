7f943cccb42d5dbe1dfb4e2cf8e1e2d4348cc99a
Nguyen Do <khoanguyen.do@outlook.com>
2025-02-05 18:29:21 +0700

    feat: serve from cache while revalidate in background

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{collections::HashMap, time::Duration};
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 
 use aoi::{
     logger::SimpleLogger,
     playing::{
         cover_art_by_release_group, cover_art_by_spotify_path, now_playing, previous_listen,
+        ListenMetadata,
     },
     template::playing_template,
 };
@@ -23,9 +27,15 @@ use tera::Tera;
 use usvg::{Options, Transform, Tree};
 
 #[derive(Clone)]
+pub struct ResponseCache {
+    pub timestamp: Instant,
+    pub response: Vec<u8>,
+}
+
+#[derive(Clone)]
 pub struct AppState {
     pub tera: Tera,
-    pub response_cache: Cache<String, Vec<u8>>,
+    pub response_cache: Cache<String, ResponseCache>,
     pub cover_art_cache: Cache<String, String>,
 }
 
@@ -35,9 +45,12 @@ static LOGGER: SimpleLogger = SimpleLogger;
 async fn main() {
     let _ = log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info));
 
-    // cache response data for 1 minute
     let response_cache = Cache::builder()
-        .time_to_live(Duration::from_secs(60))
+        .weigher(|_key, value: &ResponseCache| -> u32 {
+            value.response.len().try_into().unwrap_or(u32::MAX)
+        })
+        // This cache will hold up to 64MiB of values.
+        .max_capacity(64 * 1024 * 1024)
         .build();
     // cache cover art data for 1 day
     let cover_art_cache = Cache::builder()
@@ -71,6 +84,8 @@ async fn main() {
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    info!("App started!");
 }
 
 async fn get_playing_now(
@@ -78,39 +93,117 @@ async fn get_playing_now(
     Path(id): Path<String>,
     Query(params): Query<HashMap<String, String>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    const WIDTH: i32 = 1000;
-    const HEIGHT: i32 = 200;
+    let color_mode: Option<String> = params.get("color_mode").cloned();
+    let fill = params.get("fill").cloned();
+    let transparent = params.get("transparent").cloned();
 
-    let color_mode = params.get("color_mode");
-    let fill = params.get("fill");
-    let transparent = params.get("transparent");
-
-    match state
+    if let Some(val) = state
         .response_cache
         .get(&format!(
             "{}-{}-{}-{}",
             id,
-            color_mode.unwrap_or(&String::new()),
-            fill.unwrap_or(&String::new()),
-            transparent.unwrap_or(&String::new())
+            color_mode.clone().unwrap_or("default".to_string()),
+            fill.clone().unwrap_or("default".to_string()),
+            transparent.clone().unwrap_or("default".to_string())
         ))
         .await
     {
-        Some(val) => {
-            println!("Cache HIT, user {}", id);
-            return Ok((
-                [
-                    (header::CONTENT_TYPE, "image/png"),
-                    (
-                        header::CONTENT_DISPOSITION,
-                        "inline; filename=\"now-playing.png\"",
-                    ),
-                ],
-                val,
-            ));
+        info!("Cache HIT, user {}", id);
+
+        if Instant::now().duration_since(val.timestamp).as_secs() > (60 * 1) {
+            tokio::spawn(async move {
+                info!("Revalidating listen of user {}", id);
+                let result = render_listen(
+                    &state,
+                    &id,
+                    color_mode.as_ref(),
+                    fill.as_ref(),
+                    transparent.as_ref(),
+                )
+                .await
+                .unwrap();
+
+                state
+                    .response_cache
+                    .insert(
+                        format!(
+                            "{}-{}-{}-{}",
+                            id,
+                            color_mode.unwrap_or("default".to_string()),
+                            fill.unwrap_or("default".to_string()),
+                            transparent.unwrap_or("default".to_string())
+                        ),
+                        ResponseCache {
+                            timestamp: Instant::now(),
+                            response: result.clone(),
+                        },
+                    )
+                    .await;
+            });
         }
-        None => info!("Cache MISS, user {}", id),
+
+        return Ok((
+            [
+                (header::CONTENT_TYPE, "image/png"),
+                (
+                    header::CONTENT_DISPOSITION,
+                    "inline; filename=\"now-playing.png\"",
+                ),
+            ],
+            val.response,
+        ));
     };
+
+    let result = render_listen(
+        &state,
+        &id,
+        color_mode.as_ref(),
+        fill.as_ref(),
+        transparent.as_ref(),
+    )
+    .await
+    .unwrap();
+
+    state
+        .response_cache
+        .insert(
+            format!(
+                "{}-{}-{}-{}",
+                id,
+                color_mode.unwrap_or("default".to_string()),
+                fill.unwrap_or("default".to_string()),
+                transparent.unwrap_or("default".to_string())
+            ),
+            ResponseCache {
+                timestamp: Instant::now(),
+                response: result.clone(),
+            },
+        )
+        .await;
+
+    Ok((
+        [
+            (header::CONTENT_TYPE, "image/png"),
+            (
+                header::CONTENT_DISPOSITION,
+                "inline; filename=\"now-playing.png\"",
+            ),
+        ],
+        result,
+    ))
+}
+
+pub async fn render_listen(
+    state: &AppState,
+    id: &String,
+    color_mode: Option<&String>,
+    fill: Option<&String>,
+    transparent: Option<&String>,
+) -> Result<Vec<u8>, String> {
+    const WIDTH: i32 = 1000;
+    const HEIGHT: i32 = 200;
+
+    info!("Getting listen of user {}", id);
 
     let client = Client::new();
 
@@ -118,64 +211,13 @@ async fn get_playing_now(
         Ok(val) => (val, true),
         Err(_) => match previous_listen(&client, &id).await {
             Ok(val) => (val, false),
-            Err(err) => {
-                return Err((
-                    StatusCode::BAD_REQUEST,
-                    format!("User has no listen history, error: {:#?}", err),
-                ))
-            }
+            Err(err) => return Err(format!("User has no listen history, error: {:#?}", err)),
         },
     };
 
     let image = if let Some(metadata) = listen.metadata {
-        if let Some(release_group) = metadata.release_group {
-            match state.cover_art_cache.get(&release_group).await {
-                Some(val) => {
-                    info!(
-                        "Cache HIT, getting cover art of release group #{}",
-                        &release_group
-                    );
-                    Some(val)
-                }
-                None => match cover_art_by_release_group(&release_group).await {
-                    Ok(val) => {
-                        info!(
-                            "Cache MISS, inserting cover art of release group #{}",
-                            &release_group
-                        );
-                        state
-                            .cover_art_cache
-                            .insert(release_group.clone(), val.clone())
-                            .await;
-                        Some(val)
-                    }
-                    Err(_err) => None,
-                },
-            }
-        } else if let Some(spotify_path) = metadata.spotify_path {
-            match state.cover_art_cache.get(&spotify_path).await {
-                Some(val) => {
-                    info!(
-                        "Cache HIT, getting cover art of Spotify path [{}]",
-                        &spotify_path
-                    );
-                    Some(val)
-                }
-                None => match cover_art_by_spotify_path(&spotify_path).await {
-                    Ok(val) => {
-                        info!(
-                            "Cache MISS, inserting cover art of Spotify path [{}]",
-                            &spotify_path
-                        );
-                        state
-                            .cover_art_cache
-                            .insert(spotify_path.clone(), val.clone())
-                            .await;
-                        Some(val)
-                    }
-                    Err(_err) => None,
-                },
-            }
+        if let Ok(result) = get_cover_art(&metadata, &state.cover_art_cache).await {
+            result
         } else {
             None
         }
@@ -209,28 +251,58 @@ async fn get_playing_now(
 
     let result = pixmap.encode_png().unwrap();
 
-    state
-        .response_cache
-        .insert(
-            format!(
-                "{}-{}-{}-{}",
-                id,
-                color_mode.unwrap_or(&String::new()),
-                fill.unwrap_or(&String::new()),
-                transparent.unwrap_or(&String::new())
-            ),
-            result.clone(),
-        )
-        .await;
+    Ok(result)
+}
 
-    Ok((
-        [
-            (header::CONTENT_TYPE, "image/png"),
-            (
-                header::CONTENT_DISPOSITION,
-                "inline; filename=\"now-playing.png\"",
-            ),
-        ],
-        result,
-    ))
+async fn get_cover_art(
+    metadata: &ListenMetadata,
+    cache: &Cache<String, String>,
+) -> Result<Option<String>, String> {
+    let result = if let Some(release_group) = &metadata.release_group {
+        match cache.get(release_group).await {
+            Some(val) => {
+                info!(
+                    "Cache HIT, getting cover art of release group #{}",
+                    &release_group
+                );
+                Some(val)
+            }
+            None => match cover_art_by_release_group(&release_group).await {
+                Ok(val) => {
+                    info!(
+                        "Cache MISS, inserting cover art of release group #{}",
+                        &release_group
+                    );
+                    cache.insert(release_group.clone(), val.clone()).await;
+                    Some(val)
+                }
+                Err(_err) => None,
+            },
+        }
+    } else if let Some(spotify_path) = &metadata.spotify_path.clone() {
+        match cache.get(spotify_path).await {
+            Some(val) => {
+                info!(
+                    "Cache HIT, getting cover art of Spotify path [{}]",
+                    &spotify_path
+                );
+                Some(val)
+            }
+            None => match cover_art_by_spotify_path(&spotify_path).await {
+                Ok(val) => {
+                    info!(
+                        "Cache MISS, inserting cover art of Spotify path [{}]",
+                        &spotify_path
+                    );
+                    cache.insert(spotify_path.clone(), val.clone()).await;
+                    Some(val)
+                }
+                Err(_err) => None,
+            },
+        }
+    } else {
+        None
+    };
+
+    Ok(result)
 }
